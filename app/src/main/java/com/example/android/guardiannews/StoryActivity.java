c93163286b4b531bb524f5dd6bc2a68083edff1e
Adam <datriphopper@gmail.com>
2018-07-27 15:45:46 -0500

    Updated StoryActvity with private variables to be used later

--- app/src/main/java/com/example/android/guardiannews/StoryActivity.java
+++ app/src/main/java/com/example/android/guardiannews/StoryActivity.java
@@ -2,8 +2,20 @@ package com.example.android.guardiannews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StoryActivity extends AppCompatActivity {
+    //URL to access API
+    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search?q=smartphone&api-key=c8703a02-b362-46bc-a071-be6ec2eca354";
+
+    //Constant int value for loader
+    private final int STORY_LOADER_ID = 1;
+
+    //Adapter instance
+    private StoryAdapter mAdapter;
+
+    //TextView for EmptyState
+    private TextView mEmptyStateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
