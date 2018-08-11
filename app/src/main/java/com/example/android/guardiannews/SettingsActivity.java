064ce4d06dc35c558201adfd3f6262726a8d8411
Adam <datriphopper@gmail.com>
2018-08-11 11:40:36 -0500

    Added SettingsActivity class and layout file and Manifest entry

---
+++ app/src/main/java/com/example/android/guardiannews/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.guardiannews;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class StoryPreferenceFragment extends PreferenceFragment {
+
+    }
+
+}
