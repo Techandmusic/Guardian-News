fa906ab5b250da9ad21d412a4ece19e237f0a086
Adam <datriphopper@gmail.com>
2018-08-11 12:03:27 -0500

    Added menu in the app bar of StoryActivity

--- app/src/main/java/com/example/android/guardiannews/StoryActivity.java
+++ app/src/main/java/com/example/android/guardiannews/StoryActivity.java
@@ -10,6 +10,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -95,6 +97,23 @@ public class StoryActivity extends AppCompatActivity implements LoaderManager.Lo
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
